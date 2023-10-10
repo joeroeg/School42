aac3693e5035e1016ef51bfc27837c53c6126453
joeroeg <6369633@gmail.com>
2023-10-09 23:22:00 -0400

    update

--- playground/codewars/no_zeroes_for_heroes.c
+++ playground/codewars/no_zeroes_for_heroes.c
@@ -13,25 +13,27 @@ gcc practice.c -lm && ./a.out
 */
 
 #include <stdio.h>
-#include <math.h>
+
+int no_boring_zeros(int n) {
+    if (n == 0)  // Return zero as is
+        return 0;
+
+    while (n % 10 == 0)
+        n /= 10;
+
+    return n;
+}
 
 int main() {
-    int number = 123456789;
-    int newNumber = 0;
-    int digitPosition = 1;  // To keep track of the current digit position
+    int num1 = 1450;
+    int num2 = 960000;
+    int num3 = 1050;
+    int num4 = -1050;
 
-    while (number > 0) {
-        int digit = number % 10;
-        if (digitPosition == 4) {
-            digit = 0;  // Replace with the desired new value
-        } else if (digitPosition == 7) {
-            digit = 0;  // Replace with the desired new value
-        }
-        newNumber = newNumber + digit * pow(10, digitPosition - 1);
-        number /= 10;
-        digitPosition++;
-    }
+    printf("%d -> %d\n", num1, no_boring_zeros(num1));
+    printf("%d -> %d\n", num2, no_boring_zeros(num2));
+    printf("%d -> %d\n", num3, no_boring_zeros(num3));
+    printf("%d -> %d\n", num4, no_boring_zeros(num4));
 
-    printf("Modified number: %d\n", newNumber);
     return 0;
 }
--- practice.c
+++ practice.c
@@ -1,14 +1,29 @@
-/*
-Numbers ending with zeros are boring.
-They might be fun in your world, but not here.
-Get rid of them. Only the ending ones.
+#include <stdio.h>
 
-1450 -> 145
-960000 -> 96
-1050 -> 105
--1050 -> -105
-Zero alone is fine, don't worry about it. Poor guy anyway
-*/
+int no_boring_zeros(int n) {
+    if (n == 0)  // Return zero as is
+        return 0;
+
+    while (n % 10 == 0)
+        n /= 10;
+
+    return n;
+}
+
+int main() {
+    int num1 = 1450;
+    int num2 = 960000;
+    int num3 = 1050;
+    int num4 = -1050;
+
+    printf("%d -> %d\n", num1, no_boring_zeros(num1));
+    printf("%d -> %d\n", num2, no_boring_zeros(num2));
+    printf("%d -> %d\n", num3, no_boring_zeros(num3));
+    printf("%d -> %d\n", num4, no_boring_zeros(num4));
+
+    return 0;
+}
+
 
 #include <stdio.h>
 #include <math.h>
@@ -18,34 +33,17 @@ int main() {
     int newNumber = 0;
     int digitPosition = 1;  // To keep track of the current digit position
 
-    while (number > 0)
-    {
+    while (number > 0) {
         int digit = number % 10;
-        
-        
-        if (digitPosition == 4)
-        {
+        if (digitPosition == 4) {
+            digit = 0;  // Replace with the desired new value
+        } else if (digitPosition == 7) {
             digit = 0;  // Replace with the desired new value
         }
-        else if (digitPosition == 7)
-        {
-            digit = 0;  // Replace with the desired new value
-        }
-
-
-        // newNumber = newNumber + digit * pow(10, digitPosition - 1);
-        newNumber = newNumber * 10 + digit;
+        newNumber = newNumber + digit * pow(10, digitPosition - 1);
         number /= 10;
         digitPosition++;
     }
-
-    //     // Reverse the newNumber
-    // int reversedNumber = 0;
-    // while (newNumber > 0) {
-    //     int digit = newNumber % 10;
-    //     reversedNumber = reversedNumber * 10 + digit;
-    //     newNumber /= 10;
-    // }
 
     printf("Modified number: %d\n", newNumber);
     return 0;
