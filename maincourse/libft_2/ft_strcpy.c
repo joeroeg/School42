f71517c16fd57325193ceb6d2c610ae72fa2690b
joeroeg <6369633@gmail.com>
2023-10-11 22:39:18 -0400

    up

--- maincourse/libft_2/ft_strcpy.c
+++ maincourse/libft_2/ft_strcpy.c
@@ -11,20 +11,16 @@ char    *ft_strcpy(char *s1, const char *s2)
             s1++;
             s2++;
         }
-        // if string is not null terminated then start_s1 will point to -> Hello, world!Hello, world!
         *s1 = '\0';
     return (start_s1);
 }
 
 int main()
 {
-    int i = 0;
-    char source[] = "Hello, world!";  // Source string
-    char destination[13];  // Destination buffer of insufficient size
+    char source[] = "Hello, world!";
+    char destination[13];
 
     ft_strcpy(destination, source);
-
-    // Printing the destination string
     printf("Destination: %s\n", destination);
 
     return 0;
