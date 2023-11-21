faeafd8d3382853145bebfbaa4c521d9462af0e0
joeroeg <6369633@gmail.com>
2023-11-21 17:43:13 -0500

    up

--- maincourse/42exams/exam02/level_1/rot_13.c
+++ maincourse/42exams/exam02/level_1/rot_13.c
@@ -1,31 +1,21 @@
 #include <unistd.h>
 
-/*
-void rot_13(char *str) {
-    while (*str != '\0') {
-        char c = *str;
-        if ((c >= 'a' && c <= 'm') || (c >= 'A' && c <= 'M')) {
-            c += 13;
-        } else if ((c >= 'n' && c <= 'z') || (c >= 'N' && c <= 'Z')) {
-            c -= 13;
-        }
-        write(1, &c, 1);
-        str++;
-    }
-}
-
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        write(1, "\n", 1);
-    } else {
-        rot_13(argv[1]);
-        write(1, "\n", 1);
-    }
-    return 0;
-}
-*/
-
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
+    int i = 0;
+    char c;
 
+    if (argc == 2)
+    {
+        while ((c = argv[1][i]) != '\0')
+        {
+        if ((c >= 'a' && c <= 'm') || (c >= 'A' && c <= 'M'))
+            c += 13;
+        else if ((c >= 'n' && c <= 'z') || (c >= 'N' && c <= 'Z'))
+            c -= 13;
+        write(1, &c, 1);
+            i++;
+        }
+    }
+    write(1, "\n", 1);
 }
