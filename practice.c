21b4a32e8566f6c2cae05e26ce546446c73b8a9f
joeroeg <6369633@gmail.com>
2023-12-23 18:34:28 -0500

    up

--- practice.c
+++ practice.c
@@ -1,32 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-int ft_isspace(char c)
-{
-    return (c == ' ' || c == '\t' || c == '\n');
-}
-
-
-int word_count(char *str)
-{
-    int inword = 0;
-    int words = 0;
-    for (int i = 0; str[i]; i++)
-    {
-        if (!ft_isspace(str[i]) && inword == 0)
-        {
-            words++;
-            inword = 1;
-        }
-        else if (ft_isspace(str[i]))
-            inword = 0;
-    }
-    return (words);
-}
 
 int main()
 {
-    char *str = "abc def ghi";
-    printf("%d", word_count(str));
+	printf("hello world");
 }
+
