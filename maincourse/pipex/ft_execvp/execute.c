a50f4c126e553f7bf0ed6d901e3d7e769f8508d0
joeroeg <6369633@gmail.com>
2023-12-28 15:18:43 -0500

    up

--- maincourse/pipex/ft_execvp/execute.c
+++ maincourse/pipex/ft_execvp/execute.c
@@ -26,30 +26,35 @@ char **parse_command(const char *cmd) {
     char quote_char = '\0';
     char *arg_start = temp;
 
-    for (char *p = temp; *p != '\0'; p++) {
-        // Handle entering and exiting quotes
-        if ((*p == '\'' || *p == '"') && (!in_quotes || *p == quote_char)) {
-            if (in_quotes) {
-                in_quotes = 0;
-                *p = '\0';  // Null-terminate the argument
-                if (i < MAX_ARGS) {
-                    argv[i++] = strdup(arg_start);
-                }
-            } else {
-                in_quotes = 1;
-                quote_char = *p;
-                arg_start = p + 1;  // Start after the quote
-            }
-        } else if (*p == ' ' && !in_quotes) {
+for (char *p = temp; *p != '\0'; p++) {
+    // printf("Current char: %c\n", *p);
+    // Handle entering and exiting quotes
+    if ((*p == '\'' || *p == '"') && (!in_quotes || *p == quote_char)) {
+        if (in_quotes) {
+            in_quotes = 0;
             *p = '\0';  // Null-terminate the argument
-            if (arg_start != p) {  // Avoid empty arguments
-                if (i < MAX_ARGS) {
-                    argv[i++] = strdup(arg_start);
-                }
+            if (i < MAX_ARGS) {
+                argv[i] = strdup(arg_start);
+                // printf("Argument found: %s\n", argv[i]);  // Print the argument found
+                i++;
             }
-            arg_start = p + 1;
+        } else {
+            in_quotes = 1;
+            quote_char = *p;
+            arg_start = p + 1;  // Start after the quote
         }
+    } else if (*p == ' ' && !in_quotes) {
+        *p = '\0';  // Null-terminate the argument
+        if (arg_start != p) {  // Avoid empty arguments
+            if (i < MAX_ARGS) {
+                argv[i] = strdup(arg_start);
+                // printf("Argument found: %s\n", argv[i]);  // Print the argument found
+                i++;
+            }
+        }
+        arg_start = p + 1;
     }
+}
 
     if (in_quotes) {
         fprintf(stderr, "Unmatched quote in command\n");
@@ -59,17 +64,20 @@ char **parse_command(const char *cmd) {
         return NULL;
     }
 
-    // Add the last argument if any and not in quotes
-    if (*arg_start != '\0' && !in_quotes && i < MAX_ARGS) {
-        argv[i++] = strdup(arg_start);
-    }
+	// Handle last argument outside the loop
+	if (*arg_start != '\0' && !in_quotes && i < MAX_ARGS) {
+    argv[i] = strdup(arg_start);
+    // printf("Argument found: %s\n", argv[i]);  // Print the argument found
+    i++;
+	}
 
     argv[i] = NULL; // NULL-terminate the array
     free(temp);
     return argv;
 }
 
-int ft_execvp(const char *file, char *const argv[], char *const envp[]) {
+int ft_execvp2(const char *file, char *const argv[], char *const envp[]) {
+
     const char *path_const = NULL;
     for (int i = 0; envp[i] != NULL; i++) {
         if (strncmp(envp[i], "PATH=", 5) == 0) {
@@ -123,8 +131,67 @@ int ft_execvp(const char *file, char *const argv[], char *const envp[]) {
     return -1;
 }
 
+int ft_execvp(const char *file, char *const argv[], char *const envp[]) {
+    char *path_env = NULL;
+    for (int i = 0; envp[i] != NULL; i++) {
+        if (strncmp(envp[i], "PATH=", 5) == 0) {
+            path_env = envp[i] + 5; // Skip the "PATH=" part
+            break;
+        }
+    }
+
+    if (path_env == NULL) {
+        fprintf(stderr, "PATH not found\n");
+        return -1;
+    }
+
+    char *path = strdup(path_env); // Duplicate the PATH string for strtok
+    if (path == NULL) {
+        perror("strdup");
+        return -1;
+    }
+
+    char fullPath[1024];
+    char *dir = strtok(path, ":");
+    while (dir != NULL) {
+        snprintf(fullPath, sizeof(fullPath), "%s/%s", dir, file);
+        if (access(fullPath, X_OK) == 0) {
+            // Execute the command
+            pid_t pid = fork();
+            if (pid == 0) {
+                execv(fullPath, argv);
+                perror("execv");
+                exit(EXIT_FAILURE);
+            } else if (pid > 0) {
+                int status;
+                waitpid(pid, &status, 0);
+                free(path); // Free the duplicated string
+                return status;
+            } else {
+                perror("fork");
+                free(path); // Free the duplicated string
+                return -1;
+            }
+        }
+        dir = strtok(NULL, ":");
+    }
+
+    free(path); // Free the duplicated string
+    fprintf(stderr, "%s: command not found\n", file);
+    return -1;
+}
+
+
+// int main(int argc, char **argv, char **envp) {
+//     char **cmd1_args = parse_command(argv[1]);
+//     for (int i = 0; cmd1_args[i] != NULL; i++) {
+//         printf("token [%d]: '%s'\n", i, cmd1_args[i]);
+//     }
+//     ft_execvp(argv[1], cmd1_args, envp);
+// }
 
 int main(int argc, char **argv, char **envp) {
-    char **cmd1_args = parse_command(argv[1]);
-	ft_execvp(argv[1], cmd1_args, envp);
+    char *cmd1_args[] = {"/bin/ls", "-l", NULL};
+    ft_execvp(cmd1_args[0], cmd1_args, envp);
 }
+
