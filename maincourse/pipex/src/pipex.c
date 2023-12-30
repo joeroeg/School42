76ecd30456c6b71727aa8de6a8f4bdc6af4db8be
joeroeg <6369633@gmail.com>
2023-12-30 11:42:52 -0500

    up

--- maincourse/pipex/src/pipex.c
+++ maincourse/pipex/src/pipex.c
@@ -1,31 +1,32 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   pipex.c                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hezhukov <hezhukov@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/29 11:57:55 by hezhukov          #+#    #+#             */
-/*   Updated: 2023/12/29 20:04:34 by hezhukov         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "../pipex.h"
 
 /*
-valgrind --leak-check=full ./pipex infile "cat -e" "sort -r" outfile 2>&1 | grep "definitely lost:"
+main
+	parse_command
+		allocate space for tokens
+		split tokens
+		store tokens
+	fork first command
+		redirect input to a file (infile)
+		redirect output to a pipe[w]
+		execute ft_execvp
+			find_path_env
+			tokenize_path
+			build_full_path
+			executable test
+				execute_command
+			command_not_found
+	fork second command
+		redirect input to a pipe[r]
+		redirect output to a file (outfile)
+		execute ft_execvp
+			find_path_env
+			tokenize_path
+			build_full_path
+			executable test
+				execute_command
+			command_not_found
 */
-
-int	validate_arguments(int argc)
-{
-	if (argc != 5)
-	{
-		ft_putstr_fd("Usage: [program] [input_file] \
-			[command1] [command2] [output_file]\n", 2);
-		return (1);
-	}
-	return (0);
-}
 
 char	**parse_command(char *cmd)
 {
@@ -51,88 +52,89 @@ char	**parse_command(char *cmd)
 	return (argv);
 }
 
-pid_t	exec_cmd1(char *infile, char **cmd_args, char **envp, int pipe_fds[2])
-{
-	pid_t	cmd1;
-	int		infile_fd;
-
-	cmd1 = fork();
-	if (cmd1 == -1)
-		error_message("fork", 1);
-	if (cmd1 == 0)
-	{
-		close(pipe_fds[0]);
-		infile_fd = open(infile, O_RDONLY);
-		if (infile_fd == -1)
-			error_message("open", 1);
-
-		if (dup2(infile_fd, STDIN_FILENO) == -1)
-			error_message("dup2", 1);
-		close(infile_fd);
-
-		if (dup2(pipe_fds[1], STDOUT_FILENO) == -1)
-			error_message("dup2", 1);
-		close(pipe_fds[1]);
-
-		if (ft_execvp(cmd_args[0], cmd_args, envp) == -1)
-			error_message("execve", 1);
-	}
-	return (cmd1);
-}
-
-pid_t	exec_cmd2(char *outfile, char **cmd_args, char **envp, int pipe_fds[2])
-{
-	pid_t	cmd2;
-	int		outfile_fd;
-
-	cmd2 = fork();
-	if (cmd2 == -1)
-		error_message("fork", 1);
-	if (cmd2 == 0)
-	{
-		close(pipe_fds[1]);
-		if (dup2(pipe_fds[0], STDIN_FILENO) == -1)
-			// error_message("dup2", 1); //  <- the problem with this error_message function
-		close(pipe_fds[0]);
-		outfile_fd = open(outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		if (outfile_fd == -1)
-			error_message("open", 1);
-		if (dup2(outfile_fd, STDOUT_FILENO) == -1)
-			error_message("dup2(outfile, stdout)", 1);
-		close(outfile_fd);
-		if (ft_execvp(cmd_args[0], cmd_args, envp) == -1)
-			error_message("execvp", 1);
-	}
-	return (cmd2);
-}
-
 int	main(int argc, char **argv, char **envp)
 {
 	char	**cmd1_args;
 	char	**cmd2_args;
-	int		pipe_fds[2];
-	pid_t	cmd1;
-	pid_t	cmd2;
 
-	if (validate_arguments(argc) != 0)
-		return (1);
+	if (argc != 5)
+		return (0);
 
 	cmd1_args = parse_command(argv[2]);
 	cmd2_args = parse_command(argv[3]);
 
-	if (pipe(pipe_fds) == -1)
-		error_message("pipe", 1);
-	cmd1 = exec_cmd1(argv[1], cmd1_args, envp, pipe_fds);
-	if (cmd1 < 0)
-		return (1);
-	cmd2 = exec_cmd2(argv[4], cmd2_args, envp, pipe_fds);
-	if (cmd2 < 0)
-		return (1);
-	close(pipe_fds[0]);
-	close(pipe_fds[1]);
-	waitpid(cmd1, NULL, 0);
-	waitpid(cmd2, NULL, 0);
-	free_string_array(cmd1_args);
-	free_string_array(cmd2_args);
-	return (0);
+    int pipe_fds[2];
+    if (pipe(pipe_fds) == -1) {
+        perror("pipe");
+        return 1;
+    }
+
+    pid_t cmd1, cmd2;
+
+    // Fork for the first command
+    cmd1 = fork();
+    if (cmd1 == -1) {
+        perror("fork");
+        return 1;
+    }
+
+    if (cmd1 == 0) {
+        int infile_fd = open(argv[1], O_RDONLY);
+        if (infile_fd == -1) {
+            perror("open");
+            exit(EXIT_FAILURE);
+        }
+        dup2(infile_fd, STDIN_FILENO);
+        close(infile_fd);
+
+        dup2(pipe_fds[1], STDOUT_FILENO);
+        close(pipe_fds[0]);
+        close(pipe_fds[1]);
+
+        // ft_execvp(argv[2], cmd1_args, envp);
+        // ft_execvp(argv[2], &argv[2], envp);
+		ft_execvp(cmd1_args[0], cmd1_args, envp);
+
+        exit(EXIT_FAILURE);
+    }
+
+    // Fork for the second command
+    cmd2 = fork();
+    if (cmd2 == -1) {
+        perror("fork");
+        return 1;
+    }
+
+    if (cmd2 == 0) {
+        close(pipe_fds[1]);
+        dup2(pipe_fds[0], STDIN_FILENO);
+        close(pipe_fds[0]);
+
+        int outfile_fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (outfile_fd == -1) {
+            perror("open");
+            exit(EXIT_FAILURE);
+        }
+        dup2(outfile_fd, STDOUT_FILENO);
+        close(outfile_fd);
+
+        // ft_execvp(argv[3], cmd2_args, envp);
+        // ft_execvp(argv[3], &argv[3], envp);
+		ft_execvp(cmd2_args[0], cmd2_args, envp);
+        exit(EXIT_FAILURE);
+    }
+    for (int i = 0; cmd1_args[i] != NULL; i++) free(cmd1_args[i]);
+    free(cmd1_args);
+
+    for (int i = 0; cmd2_args[i] != NULL; i++) free(cmd2_args[i]);
+    free(cmd2_args);
+
+    // Parent process
+    close(pipe_fds[0]);
+    close(pipe_fds[1]);
+
+    waitpid(cmd1, NULL, 0);
+    waitpid(cmd2, NULL, 0);
+
+    return 0;
 }
