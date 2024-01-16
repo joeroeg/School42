a561c47c1b56a01ff79750b7ffdf58eaa0830835
joeroeg <6369633@gmail.com>
2024-01-16 17:38:17 -0500

    init_pipex_data started notminette

--- maincourse/pipex/src/pipex_redirection_bonus.c
+++ maincourse/pipex/src/pipex_redirection_bonus.c
@@ -6,7 +6,7 @@
 /*   By: hezhukov <hezhukov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/14 15:43:58 by hezhukov          #+#    #+#             */
-/*   Updated: 2024/01/16 17:34:07 by hezhukov         ###   ########.fr       */
+/*   Updated: 2024/01/16 17:37:51 by hezhukov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,42 +49,32 @@ void	redirect_last_command(t_pipex_data *pipeline)
 	}
 }
 
-
-void redirect_intermediate_command(t_pipex_data *pipeline, int index) {
-    dprintf(2, "Redirecting IO for intermediate command at index: %d\n", index);
-
-    // Redirect stdin from the read end of the previous pipe
-    if (dup2(pipeline->pipefds[(index - 1) * 2], STDIN_FILENO) < 0) {
-        perror("dup2 (stdin)");
-        exit(EXIT_FAILURE);
-    }
-
-    // Redirect stdout to the write end of the next pipe
-	dprintf(2, "Redirecting stdout to fd %d\n", pipeline->pipefds[index * 2 + 1]);
-    if (dup2(pipeline->pipefds[index * 2 + 1], STDOUT_FILENO) < 0) {
-        perror("dup2 (stdout)");
-        exit(EXIT_FAILURE);
-    }
+void	redirect_intermediate_command(t_pipex_data *pipeline, int index)
+{
+	if (dup2(pipeline->pipefds[(index - 1) * 2], STDIN_FILENO) < 0)
+		error_message("dup2 (stdin)", 1);
+	if (dup2(pipeline->pipefds[index * 2 + 1], STDOUT_FILENO) < 0)
+		error_message("dup2 (stdout)", 1);
 }
 
-void redirect_here_doc(t_pipex_data *pipeline) {
-    int fd[2];
-    if (pipe(fd) < 0) {
-        perror("pipe");
-        exit(EXIT_FAILURE);
-    }
+void	redirect_here_doc(t_pipex_data *pipeline)
+{
+	int		fd[2];
+	pid_t	pid;
 
-    pid_t pid = fork();
-    if (pid == 0) { // Child process
-        close(fd[0]); // Close the read end
-        here_doc(pipeline->limiter, fd[1]); // Write to the pipe
-        close(fd[1]);
-        exit(EXIT_SUCCESS);
-    } else if (pid < 0) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-    close(fd[1]); // Parent closes the write end
-    dup2(fd[0], STDIN_FILENO); // Redirect stdin to read from the pipe
-    close(fd[0]);
+	if (pipe(fd) < 0)
+		error_message("pipe", 1);
+	pid = fork();
+	if (pid == 0)
+	{
+		close(fd[0]);
+		here_doc(pipeline->limiter, fd[1]);
+		close(fd[1]);
+		exit(EXIT_SUCCESS);
+	}
+	else if (pid < 0)
+		error_message("fork", 1);
+	close(fd[1]);
+	dup2(fd[0], STDIN_FILENO);
+	close(fd[0]);
 }
