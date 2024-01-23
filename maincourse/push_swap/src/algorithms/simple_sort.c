50e375a0b97258690eb01b4f2df2a94040eedf22
joeroeg <6369633@gmail.com>
2024-01-22 20:07:11 -0500

    running simple cases with 3 arguments        0        7        8        7        7        8 running simple cases with 4 arguments        9       10        9        9       10       10       11       11        9       12       10       10       10       12       11       10       10        9       11       11       10       11       11 running simple cases with 5 arguments       14       12       14

--- maincourse/push_swap/src/algorithms/simple_sort.c
+++ maincourse/push_swap/src/algorithms/simple_sort.c
@@ -6,7 +6,7 @@
 /*   By: hezhukov <hezhukov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 11:23:46 by hezhukov          #+#    #+#             */
-/*   Updated: 2024/01/22 18:24:33 by hezhukov         ###   ########.fr       */
+/*   Updated: 2024/01/22 20:06:14 by hezhukov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,105 +33,167 @@ static int	find_minimum(t_node **stack, int exclude_value)
 	return (min);
 }
 
-static void	sort_3(t_node **stack_a)
-{
-	t_node	*head;
-	int		min;
-	int		next_min;
+// static void	sort_3(t_node **stack_a)
+// {
+// 	t_node	*head;
+// 	int		min;
+// 	int		next_min;
 
-	head = *stack_a;
-	min = find_minimum(stack_a, -1);
-	next_min = find_minimum(stack_a, min);
-	if (is_sorted(stack_a))
-		return ;
-	if (head->index == min && head->next->index != next_min)
-	{
-		ra(stack_a);
-		sa(stack_a);
-		rra(stack_a);
-	}
-	else if (head->index == next_min)
-	{
-		if (head->next->index == min)
-			sa(stack_a);
-		else
-			rra(stack_a);
-	}
-	else
-	{
-		if (head->next->index == min)
-			ra(stack_a);
-		else
-		{
-			sa(stack_a);
-			rra(stack_a);
-		}
-	}
+// 	head = *stack_a;
+// 	min = find_minimum(stack_a, -1);
+// 	next_min = find_minimum(stack_a, min);
+// 	if (is_sorted(stack_a))
+// 		return ;
+// 	if (head->index == min && head->next->index != next_min)
+// 	{
+// 		ra(stack_a);
+// 		sa(stack_a);
+// 		rra(stack_a);
+// 	}
+// 	else if (head->index == next_min)
+// 	{
+// 		if (head->next->index == min)
+// 			sa(stack_a);
+// 		else
+// 			rra(stack_a);
+// 	}
+// 	else
+// 	{
+// 		if (head->next->index == min)
+// 			ra(stack_a);
+// 		else
+// 		{
+// 			sa(stack_a);
+// 			rra(stack_a);
+// 		}
+// 	}
+// }
+
+// static void	sort_4(t_node **stack_a, t_node **stack_b)
+// {
+// 	int	distance;
+
+// 	if (is_sorted(stack_a))
+// 		return ;
+// 	distance = find_distance(stack_a, find_minimum(stack_a, -1));
+// 	if (distance == 1)
+// 		ra(stack_a);
+// 	else if (distance == 2)
+// 	{
+// 		ra(stack_a);
+// 		ra(stack_a);
+// 	}
+// 	else if (distance == 3)
+// 		rra(stack_a);
+// 	if (is_sorted(stack_a))
+// 		return ;
+// 	pb(stack_a, stack_b);
+// 	sort_3(stack_a);
+// 	pa(stack_a, stack_b);
+// }
+
+// void	sort_5(t_node **stack_a, t_node **stack_b)
+// {
+// 	int	distance;
+
+// 	distance = find_distance(stack_a, find_minimum(stack_a, -1));
+// 	if (distance == 1)
+// 		ra(stack_a);
+// 	else if (distance == 2)
+// 	{
+// 		ra(stack_a);
+// 		ra(stack_a);
+// 	}
+// 	else if (distance == 3)
+// 	{
+// 		rra(stack_a);
+// 		rra(stack_a);
+// 	}
+// 	else if (distance == 4)
+// 		rra(stack_a);
+// 	if (is_sorted(stack_a))
+// 		return ;
+// 	pb(stack_a, stack_b);
+// 	sort_4(stack_a, stack_b);
+// 	pa(stack_a, stack_b);
+// }
+
+// void	simple_sort(t_node **stack_a, t_node **stack_b)
+// {
+// 	int	size;
+
+// 	if (is_sorted(stack_a) || ft_lstsize(*stack_a) == 0
+// 		|| ft_lstsize(*stack_a) == 1)
+// 		return ;
+// 	size = ft_lstsize(*stack_a);
+// 	if (size == 2)
+// 		sa(stack_a);
+// 	else if (size == 3)
+// 		sort_3(stack_a);
+// 	else if (size == 4)
+// 		sort_4(stack_a, stack_b);
+// 	else if (size == 5)
+// 		sort_5(stack_a, stack_b);
+// }
+
+void	sort_3(t_node **stack_a)
+{
+	t_sort_function	*jump_table;
+	int				index;
+
+	jump_table = create_jump_table();
+	index = get_sort_index(stack_a);
+	if (index != -1)
+		jump_table[index](stack_a);
 }
 
-static void	sort_4(t_node **stack_a, t_node **stack_b)
-{
-	int	distance;
-
-	if (is_sorted(stack_a))
-		return ;
-	distance = find_distance(stack_a, find_minimum(stack_a, -1));
-	if (distance == 1)
-		ra(stack_a);
-	else if (distance == 2)
-	{
-		ra(stack_a);
-		ra(stack_a);
-	}
-	else if (distance == 3)
-		rra(stack_a);
-	if (is_sorted(stack_a))
-		return ;
-	pb(stack_a, stack_b);
-	sort_3(stack_a);
-	pa(stack_a, stack_b);
+bool is_empty(t_node **stack) {
+    return (*stack == NULL);
 }
 
-void	sort_5(t_node **stack_a, t_node **stack_b)
-{
-	int	distance;
-
-	distance = find_distance(stack_a, find_minimum(stack_a, -1));
-	if (distance == 1)
-		ra(stack_a);
-	else if (distance == 2)
-	{
-		ra(stack_a);
-		ra(stack_a);
-	}
-	else if (distance == 3)
-	{
-		rra(stack_a);
-		rra(stack_a);
-	}
-	else if (distance == 4)
-		rra(stack_a);
-	if (is_sorted(stack_a))
-		return ;
-	pb(stack_a, stack_b);
-	sort_4(stack_a, stack_b);
-	pa(stack_a, stack_b);
+int find_position(t_node **stack, int value) {
+    t_node *head = *stack;
+    int position = 0;
+    while (head != NULL) {
+        if (head->index == value) {
+            return position;
+        }
+        head = head->next;
+        position++;
+    }
+    return -1; // Value not found
 }
 
-void	simple_sort(t_node **stack_a, t_node **stack_b)
-{
-	int	size;
+void move_minimum_to_top(t_node **stack_a, int min_position, int stack_size) {
+    if (min_position <= stack_size / 2) {
+        // Rotate upwards
+        for (int i = 0; i < min_position; ++i) {
+            ra(stack_a);
+        }
+    } else {
+        // Rotate downwards
+        for (int i = 0; i < (stack_size - min_position); ++i) {
+            rra(stack_a);
+        }
+    }
+}
 
-	if (is_sorted(stack_a) || ft_lstsize(*stack_a) == 0
-		|| ft_lstsize(*stack_a) == 1)
-		return ;
-	size = ft_lstsize(*stack_a);
-	if (size == 2)
-		sa(stack_a);
-	else if (size == 3)
-		sort_3(stack_a);
-	else if (size == 4)
-		sort_4(stack_a, stack_b);
-	else if (size == 5)
-		sort_5(stack_a, stack_b);
+void push_minimum_to_b(t_node **stack_a, t_node **stack_b) {
+    int min = find_minimum(stack_a, -1);
+    int min_position = find_position(stack_a, min);
+    int stack_size = ft_lstsize(*stack_a); // Assuming you have a function to get the size of the stack
+
+    move_minimum_to_top(stack_a, min_position, stack_size);
+    pb(stack_a, stack_b);
+}
+
+
+
+void simple_sort(t_node **stack_a, t_node **stack_b) {
+    while (!is_empty(stack_a)) {
+        push_minimum_to_b(stack_a, stack_b);
+    }
+    while (!is_empty(stack_b)) {
+        pa(stack_a, stack_b); // Move all elements back to stack_a
+    }
 }
---
+++ maincourse/push_swap/src/algorithms/simple_sort_3_operations.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   simple_sort_3_operations.c                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hezhukov <hezhukov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/21 18:52:14 by hezhukov          #+#    #+#             */
+/*   Updated: 2024/01/22 19:52:50 by hezhukov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../push_swap.h"
+
+void	sort_132(t_node **stack_a)
+{
+	ra(stack_a);
+	sa(stack_a);
+	rra(stack_a);
+}
+
+void	sort_213(t_node **stack_a)
+{
+	sa(stack_a);
+}
+
+void	sort_231(t_node **stack_a)
+{
+	rra(stack_a);
+}
+
+void	sort_312(t_node **stack_a)
+{
+	ra(stack_a);
+}
+
+void	sort_321(t_node **stack_a)
+{
+	sa(stack_a);
+	rra(stack_a);
+}
