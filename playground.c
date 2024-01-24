0f2fc46150a26909ac27124bd0088fa0e951716f
joeroeg <6369633@gmail.com>
2024-01-23 21:17:24 -0500

    up

--- playground.c
+++ playground.c
@@ -1,33 +1,9 @@
-#include <unistd.h>
+#include <stdio.h>
 
 int main(int argc, char **argv)
 {
-    int i = 0;
-    int seen[255] = {0};
-
-    if (argc == 3)
-    {
-        while (argv[1][i])
-        {
-			int character = argv[1][i];
-            if (seen[character] == 0)
-            {
-                seen[character] = 1;
-                write(1, &argv[1][i], 1);
-            }
-            i++;
-        }
-        i = 0;
-        while (argv[2][i])
-        {
-			int character = argv[2][i];
-            if (seen[character] == 0)
-            {
-                seen[character] = 1;
-                write(1, &argv[2][i], 1);
-            }
-            i++;
-        }
-    }
-    write(1, "\n", 1);
+	for (int i = 0; i < argc; i++)
+	{
+		printf("argc[%d] argv[%s]\n", i, *argv++);
+	}
 }
