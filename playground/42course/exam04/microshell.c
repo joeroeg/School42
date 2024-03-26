c6992256d1ce25ede7223c0d398e06dc4fd96bd7
joeroeg <6369633@gmail.com>
2024-03-26 19:38:40 -0400

    update

--- playground/42course/exam04/microshell.c
+++ playground/42course/exam04/microshell.c
@@ -1,8 +1,9 @@
 #include <unistd.h>
 #include <string.h>
+#include <stdio.h>
 #include <sys/wait.h>
 
-void    perr(char *str)
+void    error(char *str)
 {
     while (*str)
         write(2, str++, 1);
@@ -11,9 +12,9 @@ void    perr(char *str)
 void cd(char **argv, int i)
 {
     if (i != 2)
-        return (perr("error: cd: bad arguments\n"));
+        return (error("error: cd: bad arguments\n"));
     else if (chdir(argv[1]) == -1)
-        return (perr("error: cd: cannot change directory to "), perr(argv[1]), perr("\n"));
+        return (error("error: cd: cannot change directory to "), error(argv[1]), error("\n"));
 }
 
 void exec(char **argv, char **envp, int i)
@@ -22,19 +23,19 @@ void exec(char **argv, char **envp, int i)
     int fds[2];
     int pip = (argv[i] && !strcmp(argv[i], "|")); // this is our flag for executing pipe if needed. Carefull! You must use &&
     if (pipe(fds) == -1)
-        return (perr("error: fatal\n"));
+        return (error("error: fatal\n"));
     int pid = fork();
     if (!pid)
     {
         argv[i] = 0; // we need to null terminate the argument list for execve program
         if (pip && (dup2(fds[1], 1) == -1 || close(fds[0]) || close(fds[1]))) // if pip is true then we need to redirect and close fds
-            return (perr("error: fatal\n"));
+            return (error("error: fatal\n"));
         execve(*argv, argv, envp);
-        return (perr("error: cannot execute "), perr(*argv), perr("\n"));
+        return (error("error: cannot execute "), error(*argv), error("\n"));
     }
     waitpid(pid, &status, 0);
     if (pip && (dup2(fds[0], 0) == -1 || close(fds[0]) || close(fds[1])))
-        return (perr("error: fatal\n"));
+        return (error("error: fatal\n"));
 }
 
 int main(int argc, char **argv, char **envp)
