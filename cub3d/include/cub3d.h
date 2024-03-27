07f528e5601482afd9f8993540be07cc77aab150
joeroeg <6369633@gmail.com>
2024-03-26 21:27:47 -0400

    initial update

---
+++ cub3d/include/cub3d.h
@@ -0,0 +1,15 @@
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "MLX42.h"
+# include "libft.h"
+# include <fcntl.h>
+# include <limits.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <math.h>
+
+
+#endif
---
+++ cub3d/src/main.c
@@ -0,0 +1,7 @@
+#include "cub3d.h"
+
+int main()
+{
+	printf("hello world from linux\n");
+	printf("hello world from mac\n");
+}
