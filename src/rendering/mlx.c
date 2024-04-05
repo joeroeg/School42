a6116c2dd16abab136888f93b32e2c11875c2607
gcrepin <gcrepin@student.42quebec.com>
2024-04-05 19:51:06 -0400

    Rotation

--- src/rendering/mlx.c
+++ src/rendering/mlx.c
@@ -1,10 +1,12 @@
 #include "cub3d.h"
 
 #define ZOOM 4
-#define MOVE_SPEED 0.1
+#define MOVE_SPEED 0.05
+#define ROTATE_SPEED 0.005
 
 void cub_key_hook(mlx_key_data_t key_data, void *param);
 bool cub_check_collision(t_cub *data, double x, double y);
+void cub_rotate(t_cub *data);
 
 static void	load_textures(t_cub *data)
 {
@@ -57,9 +59,24 @@ static	void	player_move(t_cub *data)
 {
 	double	new_x;
 	double	new_y;
+	double	corrected_vel_x;
+	double	corrected_vel_y;
 
-	new_x = data->player.x + data->player.vel_x * MOVE_SPEED;
-	new_y = data->player.y + data->player.vel_y * MOVE_SPEED;
+	if (data->player.vel_x == 0 && data->player.vel_y == 0)
+		return;
+	if (data->player.vel_x != 0 && data->player.vel_y != 0)
+	{
+		corrected_vel_x = data->player.vel_x / sqrt(2);
+		corrected_vel_y = data->player.vel_y / sqrt(2);
+	}
+	else
+	{
+		corrected_vel_x = data->player.vel_x;
+		corrected_vel_y = data->player.vel_y;
+	}
+	new_y = data->player.y - corrected_vel_y * MOVE_SPEED * data->player.dir_y + corrected_vel_x * MOVE_SPEED * data->player.dir_x;
+	new_x = data->player.x - corrected_vel_y * MOVE_SPEED * data->player.dir_x - corrected_vel_x * MOVE_SPEED * data->player.dir_y;
+//	new_y = data->player.y + data->player.vel_y * MOVE_SPEED * data->player.dir_y + data->player.vel_y * MOVE_SPEED * -data->player.dir_x;
 	if (!cub_check_collision(data, new_x, data->player.y))
 		data->player.x = new_x;
 	if (!cub_check_collision(data, data->player.x, new_y))
@@ -75,6 +92,9 @@ static	void	draw_player(t_cub *data)
 	x = (int) (data->player.x * ((double) WINDOW_WIDTH / ((double) MAX_MAP_WIDTH / ZOOM)));
 	y = (int) (data->player.y * ((double) WINDOW_HEIGHT / ((double)MAX_MAP_HEIGHT / ZOOM)));
 	draw_circle(data->render.screen, x, y, (WINDOW_HEIGHT / (MAX_MAP_HEIGHT / ZOOM)) / 4, 0x0000FFFF);
+	x = (int) ((data->player.x + data->player.dir_x) * ((double) WINDOW_WIDTH / ((double) MAX_MAP_WIDTH / ZOOM)));
+	y = (int) ((data->player.y + data->player.dir_y) * ((double) WINDOW_HEIGHT / ((double)MAX_MAP_HEIGHT / ZOOM)));
+	draw_circle(data->render.screen, x, y, (WINDOW_HEIGHT / (MAX_MAP_HEIGHT / ZOOM)) / 8, 0x000000FF);
 }
 
 void	fps_counter(t_cub *data)
@@ -129,8 +149,9 @@ void	mlx_render(void *ptr)
 		}
 		i++;
 	}
-	fps_counter(data);
+	cub_rotate(data);
 	draw_player(data);
+	fps_counter(data);
 }
 
 bool	cub_check_collision(t_cub *data, double x, double y)
@@ -159,24 +180,57 @@ void	cub_key_hook(mlx_key_data_t key_data, void *param)
 	}
 	if (key_data.action == MLX_PRESS)
 	{
-		if (key_data.key == MLX_KEY_UP)
+		if (((key_data.key == MLX_KEY_UP) && !mlx_is_key_down(data->render.mlx, MLX_KEY_W)) || \
+			((key_data.key == MLX_KEY_W) && !mlx_is_key_down(data->render.mlx, MLX_KEY_UP)))
 			data->player.vel_y += -1;
-		if (key_data.key == MLX_KEY_DOWN)
+		if (((key_data.key == MLX_KEY_DOWN) && !mlx_is_key_down(data->render.mlx, MLX_KEY_S)) || \
+			((key_data.key == MLX_KEY_S) && !mlx_is_key_down(data->render.mlx, MLX_KEY_DOWN)))
 			data->player.vel_y += 1;
-		if (key_data.key == MLX_KEY_LEFT)
+		if (((key_data.key == MLX_KEY_LEFT) && !mlx_is_key_down(data->render.mlx, MLX_KEY_A)) || \
+			((key_data.key == MLX_KEY_A) && !mlx_is_key_down(data->render.mlx, MLX_KEY_LEFT)))
 			data->player.vel_x += -1;
-		if (key_data.key == MLX_KEY_RIGHT)
+		if (((key_data.key == MLX_KEY_RIGHT) && !mlx_is_key_down(data->render.mlx, MLX_KEY_D)) || \
+			((key_data.key == MLX_KEY_D) && !mlx_is_key_down(data->render.mlx, MLX_KEY_RIGHT)))
 			data->player.vel_x += 1;
 	}
 	if (key_data.action == MLX_RELEASE)
 	{
-		if (key_data.key == MLX_KEY_UP)
+		if (((key_data.key == MLX_KEY_UP) && !mlx_is_key_down(data->render.mlx, MLX_KEY_W)) || \
+			((key_data.key == MLX_KEY_W) && !mlx_is_key_down(data->render.mlx, MLX_KEY_UP)))
 			data->player.vel_y -= -1;
-		if (key_data.key == MLX_KEY_DOWN)
+		if (((key_data.key == MLX_KEY_DOWN) && !mlx_is_key_down(data->render.mlx, MLX_KEY_S)) || \
+			((key_data.key == MLX_KEY_S) && !mlx_is_key_down(data->render.mlx, MLX_KEY_DOWN)))
 			data->player.vel_y -= 1;
-		if (key_data.key == MLX_KEY_LEFT)
+		if (((key_data.key == MLX_KEY_LEFT) && !mlx_is_key_down(data->render.mlx, MLX_KEY_A)) || \
+			((key_data.key == MLX_KEY_A) && !mlx_is_key_down(data->render.mlx, MLX_KEY_LEFT)))
 			data->player.vel_x -= -1;
-		if (key_data.key == MLX_KEY_RIGHT)
+		if (((key_data.key == MLX_KEY_RIGHT) && !mlx_is_key_down(data->render.mlx, MLX_KEY_D)) || \
+			((key_data.key == MLX_KEY_D) && !mlx_is_key_down(data->render.mlx, MLX_KEY_RIGHT)))
 			data->player.vel_x -= 1;
 	}
+}
+
+void	cub_rotate(t_cub *data)
+{
+	static bool	init_flag = false;
+	static int	previous_mouse_x = 0;
+	int			current_mouse_x;
+	int			delta;
+	double		hold;
+
+	if (!init_flag)
+	{
+		mlx_get_mouse_pos(data->render.mlx, &previous_mouse_x, &delta);
+		init_flag = true;
+		return;
+	}
+	mlx_get_mouse_pos(data->render.mlx, &current_mouse_x, &delta);
+	delta = current_mouse_x - previous_mouse_x;
+	if (delta != 0)
+	{
+		hold = data->player.dir_x * cos(delta * ROTATE_SPEED) - data->player.dir_y * sin(delta * ROTATE_SPEED);
+		data->player.dir_y = data->player.dir_x * sin(delta * ROTATE_SPEED) + data->player.dir_y * cos(delta * ROTATE_SPEED);
+		data->player.dir_x = hold;
+	}
+	previous_mouse_x = current_mouse_x;
 }
