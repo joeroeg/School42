fc6994f5d23efca216b1359b90b8beacd7bd3d40
joeroeg <6369633@gmail.com>
2024-04-28 17:48:09 -0400

    update

--- maincourse/CPP/05/ex01/main.cpp
+++ maincourse/CPP/05/ex01/main.cpp
@@ -3,25 +3,18 @@
 #include <iostream>
 
 int main() {
+    Bureaucrat alice("Alice", 0);
+    Form formA("Form A", 3, 5);
+
+    std::cout << formA << std::endl;
+
     try {
-        Bureaucrat alice("Alice", 0);  // This will throw an exception
-        Form formA("Form A", 3, 5);
-
-        std::cout << formA << std::endl;
-
         formA.beSigned(alice);
         std::cout << formA << std::endl;
-    } catch (const Bureaucrat::GradeTooHighException& e) {
-        std::cout << "Exception caught for Bureaucrat: " << e.what() << std::endl;
-    } catch (const Bureaucrat::GradeTooLowException& e) {
-        std::cout << "Exception caught for Bureaucrat: " << e.what() << std::endl;
-    } catch (const Form::GradeTooHighException& e) {
-        std::cout << "Exception caught for Form: " << e.what() << std::endl;
-    } catch (const Form::GradeTooLowException& e) {
-        std::cout << "Exception caught for Form: " << e.what() << std::endl;
     } catch (const std::exception& e) {
-        std::cout << "Other exceptions: " << e.what() << std::endl;
+        std::cout << "Exception: " << e.what() << std::endl;
     }
 
     return 0;
 }
+
