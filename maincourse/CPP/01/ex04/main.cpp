84f329c6659921aaa78d3af499789896b693af71
joeroeg <6369633@gmail.com>
2024-04-29 17:50:22 -0400

    update

--- maincourse/CPP/01/ex04/main.cpp
+++ maincourse/CPP/01/ex04/main.cpp
@@ -5,19 +5,19 @@ int main(int argc, char* argv[]) {
         std::cerr << "Usage: ./a.out <filename> <to_find> <replace>." << std::endl;
         return 1;
     }
-    else
-    {
-        Sed sed(argv[1]);
-        sed.replace(argv[2], argv[3]);
-    }
-
-    // try {
+    // else
+    // {
     //     Sed sed(argv[1]);
     //     sed.replace(argv[2], argv[3]);
-    // } catch (const std::exception& e) {
-    //     std::cerr << "Error: " << e.what() << std::endl;
-    //     return 1;
     // }
+
+    try {
+        Sed sed(argv[1]);
+        sed.replace(argv[2], argv[3]);
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
     return 0;
 }
 
