15e0a93da4dee94bea4a74e8f36833450304351f
joeroeg <6369633@gmail.com>
2024-05-08 18:13:33 -0400

    update

--- playground/42course/exam05/cpp_module_01/ASpell.hpp
+++ playground/42course/exam05/cpp_module_01/ASpell.hpp
@@ -4,17 +4,19 @@
 
 class ATarget;
 
-class ASpell 
+class ASpell
 {
 	protected :
 		std::string _name;
 		std::string _effects;
-	
+
 	public :
 		ASpell(std::string name, std::string effects);
-		ASpell & operator=(ASpell const & rhs);
+
 		ASpell(ASpell const & obj);
+		ASpell & operator=(ASpell const & rhs);
 		virtual ~ASpell();
+
 		std::string getName() const;
 		std::string getEffects() const;
 		virtual ASpell* clone() const = 0;
