f994113a5bcc269ece522935b66ad52618dafe31
joeroeg <6369633@gmail.com>
2024-07-25 19:50:51 -0400

    upload data science piscine

---
+++ specialization/PythonForDataScience/python-0/ex00/Hello.py
@@ -0,0 +1,17 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+
+ft_list[1] = "World"
+ft_tuple[1] = "Canada"
+ft_set.remove("tutu!")
+ft_dict["Hello"] = "Quebec"
+
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
+
