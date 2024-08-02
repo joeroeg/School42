c0fe28111690740cd8b282872f968d826e5a4fb7
joeroeg <6369633@gmail.com>
2024-08-02 00:46:41 -0400

    Refactor building.py to improve code readability and adhere to project requirements

--- specialization/PythonForDataScience/python-0/ex05/building.py
+++ specialization/PythonForDataScience/python-0/ex05/building.py
@@ -3,8 +3,12 @@ import sys
 def main():
     if len(sys.argv) == 1:
         print("What is the text to count?")
-        text = input()
-    text = sys.argv[1]
+        text = sys.stdin.readline()
+        if not text:
+            text = sys.stdin.read()
+    else:
+        text = sys.argv[1]
+
     print("The text contains", len(text), "characters:")
 
     upper = 0
@@ -13,18 +17,18 @@ def main():
     space = 0
     punctuation = 0
 
-    for i in range(len(text)):
-        if text[i].isalpha():
-            if text[i].isupper():
-                upper = upper + 1
+    for char in text:
+        if char.isalpha():
+            if char.isupper():
+                upper += 1
             else:
-                lower = lower + 1
-        elif text[i].isdigit():
-            digit = digit + 1
-        elif text[i].isspace():
-            space = space + 1
-        elif text[i] != text[i].isalnum():
-            punctuation = punctuation + 1
+                lower += 1
+        elif char.isdigit():
+            digit += 1
+        elif char.isspace() or char == '\n':
+            space += 1
+        elif not char.isalnum():
+            punctuation += 1
 
     print(upper, "upper letters")
     print(lower, "lower letters")
