96ab76d3f666ff9079e52d35bd808216b97c0f73
georgezhukov <6369633@gmail.com>
2024-09-26 21:17:54 -0400

    Add example usage for slice_me function in array2D.py

--- specialization/PythonForDataScience/python-1/ex01/array2D.py
+++ specialization/PythonForDataScience/python-1/ex01/array2D.py
@@ -4,3 +4,10 @@
 def slice_me(family: list, start: int, end: int) -> list:
     """Slices a list of family members from start to end-1 index."""
     return family[start:end]
+
+
+if __name__ == '__main__':
+    family = [[1.80, 78.4], [2.15, 102.7], [2.10, 98.5], [1.88, 75.2]]
+
+    print(slice_me(family, 0, 2))
+    print(slice_me(family, 1, -2))
