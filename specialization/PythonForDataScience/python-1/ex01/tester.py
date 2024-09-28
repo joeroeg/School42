95acfa015cdc56ca30a3eb2a6cea401bb047973c
joeroeg <6369633@gmail.com>
2024-09-28 15:54:55 -0400

    Remove unnecessary code and update family slicing function

--- specialization/PythonForDataScience/python-1/ex01/tester.py
+++ specialization/PythonForDataScience/python-1/ex01/tester.py
@@ -5,5 +5,10 @@ from array2D import slice_me
 
 family = [[1.80, 78.4], [2.15, 102.7], [2.10, 98.5], [1.88, 75.2]]
 
+print(f"My shape is : ({len(family)}, {len(family[0])})")
+print(f"My new shape is : ({len(slice_me(family, 0, 2))}, {len(family[0])})")
 print(slice_me(family, 0, 2))
+print()
+print(f"My shape is : ({len(family)}, {len(family[0])})")
+print(f"My new shape is : ({len(slice_me(family, 1, -2))}, {len(family[0])})")
 print(slice_me(family, 1, -2))
